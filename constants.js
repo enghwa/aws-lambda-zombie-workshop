bba70499c0ade9fd19b6571b1736c311172f3823
enghwa <enghwa@aws>
2016-11-15 16:49:19 +0800

    remove files that are not needed

--- constants.js
+++
@@ -1,6 +0,0 @@
-var MESSAGES_ENDPOINT = "https://idmh0bwz3d.execute-api.ap-southeast-1.amazonaws.com/ZombieWorkshopStage";
-var COGNITO_REGION = "ap-northeast-2";
-var AWS_REGION = "ap-southeast-1";
-var IDENTITY_POOL_ID = "ap-northeast-2:2bf7615e-1ef7-489a-85a9-3fac4aaeba3d";
-var USER_POOL_ID = "ap-northeast-2_8dx2H3gSr";
-var CLIENT_ID = "45063kh925kgegqu8jf7d35v96";
