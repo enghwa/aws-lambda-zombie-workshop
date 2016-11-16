1d44ed175b7514d573ada5aeae99d07ad7691470
enghwa <enghwa@aws>
2016-11-16 17:38:45 +0800

    updated

---
+++ constants.js
@@ -0,0 +1,6 @@
+var MESSAGES_ENDPOINT = "https://idmh0bwz3d.execute-api.ap-southeast-1.amazonaws.com/ZombieWorkshopStage";
+var COGNITO_REGION = "ap-northeast-2";
+var AWS_REGION = "ap-southeast-1";
+var IDENTITY_POOL_ID = "";
+var USER_POOL_ID = "ap-northeast-??";
+var CLIENT_ID = "??";
